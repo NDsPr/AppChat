816d00a8ff803a690d0976945138af0a8c79052a
NDsPr <144921933+NDsPr@users.noreply.github.com>
2024-12-29 13:04:22 +0700

    update

--- AppChatAPI/ChatAppServer.WebAPI/obj/Debug/net8.0/ChatAppServer.WebAPI.AssemblyInfo.cs
+++ AppChatAPI/ChatAppServer.WebAPI/obj/Debug/net8.0/ChatAppServer.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatAppServer.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a66426c977062fa70d1bdc2d720f019a69b004a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatAppServer.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatAppServer.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
